9ed8bab86609f0f013b911bdf15321e14e78a8b8
GeekyMovies <121683432+GeekyMovies@users.noreply.github.com>
2023-01-09 23:26:36 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,11 +25,11 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5915563603:AAE1STI93wsA7MK9pma-MOKXU4Gww5KxwWk"
 
     # Get these values from my.telegram.org
-    API_ID = 1234567
-    API_HASH = ""
+    API_ID = "23311160"
+    API_HASH = "2a1366013eca4256bce853346dbcda49"
 
     # No need to change
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -40,11 +40,11 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     # TG Ids
-    LOG_CHANNEL = -1007119867241
-    OWNER_ID = 1288398723
+    LOG_CHANNEL = -1001873970692
+    OWNER_ID = 5691018873
 
     # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
+    BOT_USERNAME = "geekyurl_bot"
 
     # auth users
     AUTH_USERS = [OWNER_ID]
